fef38ec9ebb0aeb9eb91aa757d3a493cb088efea
Kraskin-Websites <kraskin17@outlook.com>
2026-07-27 20:25:53 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
 // URL DO SERVIDOR PIX
 const PIX_SERVER_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' || !window.location.hostname || window.location.protocol === 'file:'
   ? 'http://localhost:3001'
-  
+  : 'https://cardapio-online-tcc-ii.onrender.com';
 
 // STATUS
 let cart = JSON.parse(localStorage.getItem('cart') || '[]');
